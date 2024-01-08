365487cb2c9595323cb7b216cfc24572454aff61
Jean Boussier <jean.boussier@gmail.com>
2024-01-08 11:33:49 +0100

    Measure impact of thread count on metrics
    Spreadsheet: https://docs.google.com/spreadsheets/d/1EFdgQopAMDR5zC36ZkXLlAebrI2skzpe155SgC1TCSc/edit?usp=sharing
    
    The goal is to try to measure how much is gained or lost when increasing
    or decreasing thread count, as to try to locate a decent tradeoff between
    increased throughput (cost reduction), and reduced latency (user experience).
    
    The application is configured as doing roughtly 50% IOs, with roughtly
    100ms reponse time (in practice it's more like 115).
    
    The latencies are measured in the Rack application side, as to ignore request
    queuing. Queueing shouldn't happen in production aside from breif spikes of
    traffic. If it does, it means the application is under-capacited, and capacity
    should be increased, so not a scenario we're benchmarking for.
    
    Notes.
    
    This benchmark isn't trying to acheive maximum scientificity. It's
    performed on a development machine, etc. However the number differences
    are large enough that this relatively low benchmark precision is good
    enough™ for the use case.

--- k6/constant.js
+++ k6/constant.js
@@ -5,31 +5,11 @@ export const options = {
 
   scenarios: {
     contacts: {
-      executor: 'constant-arrival-rate',
-
-      // Iterations per `timeUnit`
-      rate: 3,
-
-      // Start `startRate` iterations per timeUnit
-      timeUnit: '1s',
-
-      // Pre-allocate necessary VUs.
-      preAllocatedVUs: 10,
-
-      duration: '60s'
+      executor: 'shared-iterations',
+      vus: 4,
+      iterations: 1000,
+      maxDuration: '1m30s',
     },
-  },
-  thresholds: {
-    http_req_waiting: [
-      {
-        threshold: 'avg<375', // response time must be less than 375 ms (50% degraded or 125ms slower)
-        abortOnFail: true,
-      },
-      {
-        threshold: 'p(99)<1000', // p99 must be less than 1000 ms (~4x slower)
-        abortOnFail: true,
-      },
-    ]
   },
 };
 
--- k6/script.js
+++ k6/script.js
@@ -14,24 +14,29 @@ export const options = {
       timeUnit: '1s',
 
       // Pre-allocate necessary VUs.
-      preAllocatedVUs: 10,
+      preAllocatedVUs: 20,
 
       stages: [
         // k6 will linearly ramp up from startRate/timeUnit to this target of iterations per timeunit over the duration.
-        { target: 8, duration: '1m30s' },
+          {
+            // Using base 12 as it's easily divisible (2 * 6, 3 * 4, 4 * 3, 6 * 2)
+            // Endpoint returns in ~200ms, and we target ~80% capacity, so 4 request per second per user
+            target: 12 * 4,
+            duration: '1m'
+        },
       ],
     },
   },
   thresholds: {
     http_req_waiting: [
-      {
-        threshold: 'avg<375', // response time must be less than 375 ms (50% degraded or 125ms slower)
-        abortOnFail: true,
-      },
-      {
-        threshold: 'p(99)<1000', // p99 must be less than 1000 ms (~4x slower)
-        abortOnFail: true,
-      },
+      // {
+      //   threshold: 'avg<375', // response time must be less than 375 ms (50% degraded or 125ms slower)
+      //   abortOnFail: true,
+      // },
+      // {
+      //   threshold: 'p(99)<1000', // p99 must be less than 1000 ms (~4x slower)
+      //   abortOnFail: true,
+      // },
     ]
   },
 };
